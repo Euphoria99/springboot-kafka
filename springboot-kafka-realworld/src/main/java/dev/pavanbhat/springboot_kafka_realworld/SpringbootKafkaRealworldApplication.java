8254296ee07bd58097069cbe2b9d28975cb68a53
Euphoria99 <363pavanbhat@gmail.com>
2025-01-27 16:10:00 +0100

    new project setup

---
+++ springboot-kafka-realworld/src/main/java/dev/pavanbhat/springboot_kafka_realworld/SpringbootKafkaRealworldApplication.java
@@ -0,0 +1,13 @@
+package dev.pavanbhat.springboot_kafka_realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaRealworldApplication.class, args);
+	}
+
+}
