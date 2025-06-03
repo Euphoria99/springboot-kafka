702f3bf1a719b5a14395dee0243e1b9d099f9bb2
Euphoria99 <363pavanbhat@gmail.com>
2025-06-03 16:10:00 +0100

    kafka producer wikimedia setup

---
+++ springboot-kafka-realworld-project/kafka-producer-wikimedia/src/main/java/dev/pavanbhat/springboot/SpringbootProducerApplication.java
@@ -0,0 +1,12 @@
+package dev.pavanbhat.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerApplication.class);
+    }
+}
